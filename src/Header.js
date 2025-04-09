befe94f077f580ae807ef9a088c73ef134878ffa
tarunj45 <144881696+tarunj45@users.noreply.github.com>
2025-04-09 12:59:43 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -26,30 +26,3 @@ const Header = () => {
             </svg>
             <span className="ml-3 text-xl">Destibook</span>
           </a>
-          <nav className="md:mr-auto md:ml-8 md:py-1 flex flex-wrap items-center text-base justify-center">
-            <Link className="mr-5 hover:text-gray-300" to="/">Monuments</Link>
-            <Link className="mr-5 hover:text-gray-300" to="/explore">Restaurants</Link>
-            <Link className="mr-5 hover:text-gray-300" to="/about">About Us</Link>
-            <Link className="mr-5 hover:text-gray-300" to="/explore-unexplored">Explore</Link>
-          </nav>
-          <button className="inline-flex items-center text-white bg-gradient-to-r from-indigo-500 via-indigo-500 to-indigo-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            Explore the unexplored!
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
-        </div>
-      </header>
-    </div>
-  );
-};
-
-export default Header;
