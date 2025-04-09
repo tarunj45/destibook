08447275c5436fd8a42d9fc1c271a2072d5c8a36
tarunj45 <144881696+tarunj45@users.noreply.github.com>
2025-04-09 13:47:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,25 +9,24 @@ import Tajmahal from "./Tajmahal";
 import Hampi from "./Hampi";
 import Hawamahal from "./Hawamahal";
 import Success from "./Success";
-import Explore from "./Restaurants";
-import AboutPage from "./AboutPage";
+import Restaurants from "./Restaurants";
+import IntercomChat from "./IntercomChat"; // Optional, for chatbot
 
 function App() {
   return (
     <div className="App bg-gray-800">
+      <IntercomChat />
       <Routes>
         <Route path="/" element={[<Header />, <Hero />, <Footer />]} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/gallery/tajmahal" element={<Tajmahal />} />
-        <Route path="/gallery/hampi" element={<Hampi />} />
-        <Route path="/gallery/hawamahal" element={<Hawamahal />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/about" element={[<Header />, <AboutPage />, <Footer />]} />
+        <Route path="/gallery" element={[<Header />, <Gallery />, <Footer />]} />
+        <Route path="/gallery/tajmahal" element={[<Header />, <Tajmahal />, <Footer />]} />
+        <Route path="/gallery/hampi" element={[<Header />, <Hampi />, <Footer />]} />
+        <Route path="/gallery/hawamahal" element={[<Header />, <Hawamahal />, <Footer />]} />
+        <Route path="/success" element={[<Header />, <Success />, <Footer />]} />
+        <Route path="/explore" element={[<Header />, <Restaurants />, <Footer />]} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
